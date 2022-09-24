1a4869fa0a8ceccde5bbcf580edbbb03f4dfe168
Maksym <maksym.vak@gmail.com>
2022-09-24 14:41:52 +0300

    add testcase

--- cypress/e2e/telnyx.cy.js
+++ cypress/e2e/telnyx.cy.js
@@ -23,55 +23,55 @@ context('Actions', () => {
         cookieReset.cookieWindowReset()
     })
 
-    it('Registration on the website with invalid email', () => {
+    it('TC-01 Registration on the website with invalid email', () => {
         registrPage.clickSignup()
         registrPage.registrUser(randomdata.makeEmail(), randomdata.randomstring(10), "^"+randomdata.randomstring(25))
         registrPage.errorMessageVisible().should('be.visible')
     })
-    it('Registration on the website with valid email', () => {
+    it('TC-02 Registration on the website with valid email', () => {
         registrPage.clickSignup()
         registrPage.registrUser("ivan."+randomdata.randomstring(5)+"@gmail.com", randomdata.randomstring(10), "^"+randomdata.randomstring(25))
         registrPage.validatMessageExpect().should('have.text', "We've sent you an email to activate your account")
     })
-    it('Authorization on the website', () => {
+    it('TC-03 Authorization on the website', () => {
         loginPage.clickLogin()
         loginPage.loginUser(randomdata.makeEmail(), randomdata.randomstring(10))
         loginPage.errorMessageVisible().should('be.visible')
     })
-    it('Password reset', () => {
+    it('TC-04 Password reset', () => {
         loginPage.clickLogin()
         loginPage.clickResetPass()
         loginPage.userResetPass(randomdata.makeEmail())
         loginPage.resPassMessageVisible().should('be.visible')
     })
-    it('Voice Api section check', () => {
+    it('TC-05 Voice Api section check', () => {
         voiceapiPage.selectVoiceMenu()
         voiceapiPage.selectDropMenu()
         voiceapiPage.validatMessageExpect().should('have.text', "Voice API Pricing for Ukraine")
     })
-    it('Careers section check', () => {
+    it('TC-06 Careers section check', () => {
         careersPage.selectCareersMenu()
         careersPage.validatMessageExpect().should('have.text', "We're building the future of communications.")
         generalPage.getUrl().should('include', '/careers')
     })
-    it('Checking the Savings Calculator', () => {
+    it('TC-07 Checking the Savings Calculator', () => {
         calculatorPage.selectCalculatorMenu()
         calculatorPage.selectMessagingApi()
         calculatorPage.inputValueField(calculatorValue)
         calculatorPage.validatMessageExpect().should('include.text', "17,658")
     })
-    it('Checking the Marketplace', () => {
+    it('TC-08 Checking the Marketplace', () => {
         marketplacePage.selectMarketplaceMenu()
         marketplacePage.clickMarketplaceMenu()
         marketplacePage.validatMessageExpect().should('have.text', "Welcome to the Telnyx Integration Marketplace")
         generalPage.getUrl().should('include', 'marketplace')
     })
-    it('Sending a message using a function Talk to an expert', () => {
+    it('TC-09 Sending a message using a function Talk to an expert', () => {
         talkexpertPage.clickTalkexpert()
         talkexpertPage.fillUserForm(randomdata.randomstring(10), randomdata.randomstring(10), randomdata.makeEmail(), phon, website, randomdata.randomstring(100))
         talkexpertPage.validatMessageExpect().should('have.text', "Thanks for Reaching Out!")
     })
-    it('Products section check', () => {
+    it('TC-10 Products section check', () => {
         productsPage.selectProductsMenu()
         productsPage.validatMessageExpect().should('have.text', "Products")
     })
